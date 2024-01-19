9b258af45b7b499a0f1eca095938086da37097ba
Kalichhe <jhondh2003@gmail.com>
2024-01-18 19:20:17 -0500

    Algo mas

--- Paradigmas/Programacion Orientada A Objetos/Principios SOLID/3.LSP.py
+++ Paradigmas/Programacion Orientada A Objetos/Principios SOLID/3.LSP.py
@@ -1,2 +1,17 @@
 
 # Liskov's Substitution Principle
+
+class Ave:
+    def poner_huevos(self):
+        print("Poniendo huevos...")
+    
+    def alimentarse(self):
+        print("Alimentándose...")
+
+class AveVoladora(Ave):
+    def volar(self):
+        print("Volando...")
+
+class AveNadadora(Ave):
+    def nadar(self):
+        print("Nadando...")
